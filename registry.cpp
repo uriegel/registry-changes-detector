3de5e9d7d6652ddb743bb275eb3082682f480351
Uwe <uriegel@hotmail.de>
2022-01-14 09:08:31 +0100

    ready to publish

--- registry.cpp
+++
@@ -1,9 +0,0 @@
-
-bool WaitForChanges(uintptr_t handle) {
-    auto key = reinterpret_cast<HKEY>(handle);
-    auto status = RegNotifyChangeKeyValue(key, FALSE, REG_NOTIFY_CHANGE_LAST_SET, nullptr, FALSE);
-    return status == 0;
-}
-
-
-    
